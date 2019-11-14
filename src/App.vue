2ce9f9fd67a473310ee956efd5bb85c0ec4a471c
brian behling <brian.behling@gmail.com>
2019-11-14 11:54:31 -0700

    Initial commit for first user story

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,255 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <article class="grid">
+      <header class="grid">
+        <div class="title-container">
+          <h1>Coding Test</h1>
+        </div>
+      </header>
+      <main class="content grid" id="home-content">
+        <div class="content-title-container">
+          <h2>Accounts</h2>
+        </div>
+        <section class="grid" id="account-grid">
+          <section class="account-column grid" id="active-account-column">
+            <div class="account-container-title" id="active-account-container-title">
+              <h3>Active</h3>
+            </div>
+            <div class="account-container active-account">
+              <ul class="account-data-list" v-for="account in activeAccounts" v-bind:key="account.id">
+                <li>
+                  <label>Name:</label>
+                  {{ account.LastName }}, {{ account.FirstName }}
+                </li>
+                <li>
+                  <label>Email:</label>
+                  {{ account.Email }}
+                </li>
+                <li>
+                  <label>Phone Number:</label>
+                  {{ account.PhoneNumber | phone }}
+                </li>
+                <li>
+                  <label>Amount Due:</label>
+                  {{ account.AmountDue | currency }}
+                </li>
+                <li>
+                  <label>Due Date:</label>
+                  {{ account.PaymentDueDate | moment("MM/DD/YYYY") }}
+                </li>
+              </ul>
+            </div>
+          </section>
+          <section class="account-column grid" id="overdue-account-column">
+            <div class="account-container-title" id="overdue-account-container-title">
+              <h3>Overdue</h3>
+            </div>
+            <div class="account-container overdue-account">
+              <ul class="account-data-list" v-for="account in overdueAccounts" v-bind:key="account.id">
+                <li>
+                  <label>Name:</label>
+                  {{ account.LastName }}, {{ account.FirstName }}
+                </li>
+                <li>
+                  <label>Email:</label>
+                  {{ account.Email }}
+                </li>
+                <li>
+                  <label>Phone Number:</label>
+                  {{ account.PhoneNumber | phone }}
+                </li>
+                <li>
+                  <label>Amount Due:</label>
+                  {{ account.AmountDue | currency }}
+                </li>
+                <li>
+                  <label>Due Date:</label>
+                  {{ account.PaymentDueDate | moment("MM/DD/YYYY") }}
+                </li>
+              </ul>
+            </div>
+          </section>
+          <div class="account-column grid" id="inactive-account-column">
+            <div class="account-container-title" id="inactive-account-container-title">
+              <h3>Inactive</h3>
+            </div>
+            <div class="account-container inactive-account">
+              <ul class="account-data-list" v-for="account in inactiveAccounts" v-bind:key="account.id">
+                <li>
+                  <label>Name:</label>
+                  {{ account.LastName }}, {{ account.FirstName }}
+                </li>
+                <li>
+                  <label>Email:</label>
+                  {{ account.Email }}
+                </li>
+                <li>
+                  <label>Phone Number:</label>
+                  {{ account.PhoneNumber | phone }}
+                </li>
+                <li>
+                  <label>Amount Due:</label>
+                  {{ account.AmountDue | currency }}
+                </li>
+                <li v-if="account.PaymentDueDate">
+                  <label>Due Date:</label>
+                  {{ account.PaymentDueDate | moment("MM/DD/YYYY") }}
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer class="grid">
+        <p class="copy">&copy;</p>
+      </footer>
+    </article>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+@Component({})
+//Allow parameters without declaring their type in tsconfig
+//TypeScript types are not required for this demo
+//TODO - fully utilize TypeScript by creating Account Class and Account Interface and implement type checking
+export default class App extends Vue {
+  //set accounts to null to make it reactive
+  accounts = null;
+  mounted() {
+    axios.get("https://frontiercodingtests.azurewebsites.net/api/accounts/getall").then(response => {
+      this.accounts = response.data;
+    });
+  }
+
+  get activeAccounts() {
+    return (
+      this.accounts &&
+      this.accounts.filter(item => {
+        return item.AccountStatusId === 0;
+      })
+    );
+  }
+
+  get overdueAccounts() {
+    return (
+      this.accounts &&
+      this.accounts.filter(item => {
+        return item.AccountStatusId === 2;
+      })
+    );
+  }
+
+  get inactiveAccounts() {
+    return (
+      this.accounts &&
+      this.accounts.filter(item => {
+        return item.AccountStatusId === 1;
+      })
+    );
+  }
+}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,
+body {
+  height: 100%;
+  width: 100%;
+  font-family: Roboto, Helvetica, Arial, sans-serif;
+}
+article {
+  height: 100%;
+  grid-template-columns: 1fr;
+  grid-template-areas:
+    "header"
+    "main"
+    "footer";
+  grid-template-rows: 100px 1fr 150px;
+}
+header {
+  grid-area: header;
+  background-color: #006643;
+  color: #fff;
+  grid-template-columns: 1% 98% 1%;
+}
+h1 {
+  font-size: 3em;
+  font-weight: bold;
+}
+h2 {
+  font-size: 2em;
+  color: #006643;
+}
+h3 {
+  display: block;
+  font-size: 1.5em;
+  color: #006643;
+}
+main {
+  grid-area: main;
+}
+footer {
+  grid-area: footer;
+  background-color: #006643;
+  color: #fff;
+  grid-template-columns: 1% 98% 1%;
+  grid-template-rows: 5% 90% 5%;
+}
+.grid {
+  display: grid;
+}
+.title-container {
+  grid-column-start: 2;
+  align-self: center;
+}
+#home-content {
+  grid-row-gap: 25px;
+  grid-template-rows: 10px 100px auto;
+}
+#account-grid {
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-row-start: 3;
+}
+.account-column {
+  grid-template-rows: 2em repeat(2, 10em);
+  overflow: auto;
+  height: 100%;
+}
+.content-title-container {
+  justify-self: center;
+  height: 1em;
+  grid-row-start: 2;
+}
+#overdue-account-container-title > h3 {
+  color: #b22222;
+}
+#inactive-account-container-title > h3 {
+  color: #808080;
+}
+.account-container {
+  justify-self: center;
+  height: 5em;
+}
+.account-container-title {
+  justify-self: center;
+}
+.account-data-list {
+  list-style: none;
+}
+.account-data-list > li {
+  margin: 10px 0;
+}
+.account-data-list > li > label {
+  font-weight: bold;
+  margin: 0 5px 0 0;
+}
+.copy {
+  place-self: end;
+  grid-row-start: 2;
+  grid-column-start: 2;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,33 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
+
+Vue.use(require("vue-moment"));
+
+//TODO - reseach a good library that will contain all the filters we need
+//https://gist.github.com/kkiernan/7475b615485344f2fdf3
+Vue.filter("phone", function(phone) {
+  return phone.replace(/[^0-9]/g, "").replace(/(\d{3})(\d{3})(\d{4})/, "($1)-$2-$3");
+});
+
+//https://stackoverflow.com/questions/43208012/how-do-i-format-currencies-in-a-vue-component
+Vue.filter("currency", function(value) {
+  if (typeof value !== "number") {
+    return value;
+  }
+  var formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 0
+  });
+  return formatter.format(value);
+});
